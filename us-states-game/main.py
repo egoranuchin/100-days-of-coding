d248a4870d3f2be9a174e3152dcf44682c421e46
egoranuchin <Egoranuchin@gmail.com>
2022-12-02 13:52:55 +0100

    added minimal working functionality ready

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -10,26 +10,28 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
-
-
-while
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
-
-
-# If answer_state is one of the states in all the states of the 50_states.csv
-    #If they got it right:
-        #Create a turtle to write the name of the state at the state's x and y coordinate
-titled_answer_state = answer_state.title()
 all_states = data.state.to_list()
-if titled_answer_state in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.pu()
-    state_data = data[data.state == titled_answer_state]
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(state_data.state.item())
-    # name = writing.Writing(state.state, state.x, state.y)
+guessed_states = []
+
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+    print(answer_state)
+
+
+    # If answer_state is one of the states in all the states of the 50_states.csv
+        #If they got it right:
+            #Create a turtle to write the name of the state at the state's x and y coordinate
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.pu()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+        # name = writing.Writing(state.state, state.x, state.y)
 
 
 
